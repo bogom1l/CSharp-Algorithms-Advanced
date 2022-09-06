7dc71854844e653c69d00c5a53028b78e2723773
wavezM5 <102171820+wavezM5@users.noreply.github.com>
2022-09-06 09:45:46 +0300

    useless

---
+++ Graphs – Dijkstra and MST - Lab/testProjectNvmThis/testProjectNvmThis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace testProjectNvmThis
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = 5;
+
+            var array = new int[5];
+
+            var mylist = new List<int>();
+
+           
+            mylist.Add(1);
+
+            var b = mylist.Average();
+
+            Console.WriteLine($"{b} && {mylist.Count:f4}");
+
+
+        }
+    }
+}
